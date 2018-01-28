2f3ad89e09d612d05f7e0c2da5cb47cfd156fe3c
takashiki <857995137@qq.com>
2018-01-28 16:01:43 +0800

    up

--- app/Article.php
+++ app/Article.php
@@ -3,9 +3,25 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Prettus\Repository\Contracts\Transformable;
+use Prettus\Repository\Traits\TransformableTrait;
 use Yajra\Auditable\AuditableTrait;
 
-class Article extends Model
+/**
+ * Class Article.
+ *
+ * @package namespace App;
+ */
+class Article extends Model implements Transformable
 {
     use AuditableTrait;
+    use TransformableTrait;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [];
+
 }
