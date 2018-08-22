56ad30fbcb27b18dd49cdfaf0c737c92325b9d1f
takashiki <857995137@qq.com>
2018-08-22 15:42:58 +0800

    try to fix php-parser pack error & yarn is much faster

--- Modules/Tools/webpack.mix.js
+++ Modules/Tools/webpack.mix.js
@@ -7,6 +7,27 @@ mix.js(__dirname + '/Resources/assets/js/app.js', 'js/tools.js')
     .sass(__dirname + '/Resources/assets/sass/app.scss', 'css/tools.css')
     .extract(['vue', 'axios', 'lodash', 'element-ui'], 'js/vendor.js');
 
+mix.options({
+  processCssUrls: true,
+  purifyCss: false,
+  extractVueStyles: false,
+  imgLoaderOptions: {
+    enabled: false
+  },
+  uglify: {
+    include: /\.min\.js$/,
+    sourceMap: true,
+    uglifyOptions: {
+      compress: {
+        keep_fnames: true
+      },
+      sourceMap: true,
+      mangle: false,
+      maxLineLen: 1024
+    }
+  }
+});
+
 if (mix.inProduction()) {
     mix.version();
 }
